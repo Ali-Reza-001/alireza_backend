54ede00368b6fe68260319c7fea07ead957a5612
Ali-Reza-001 <alirezaahmadi008hero@gmail.com>
2025-09-13 21:41:19 +0430

    new

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -7,7 +7,7 @@ const allowedOrigins = [
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
+      callback(null, origin);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
