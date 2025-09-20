d355cd803a286f77d5d7de381ccc1ec019de7ccc
Ali-Reza-001 <alirezaahmadi008hero@gmail.com>
2025-09-20 15:13:37 +0430

    nu

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,8 +1,9 @@
 const User = require('../model/User');
 
 const deleteUser =  async (req, res) => {
-    const _id = req.id;
-    const data = await User.deleteOne({_id});
+    const {id} = req.body;
+    console.log(id)
+    const data = await User.deleteOne({_id : id});
     res.json({message: "User deleted successfully !", data});
 }
 
