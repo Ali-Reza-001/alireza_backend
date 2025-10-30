3894e8971965881df878c502af7eedfc36f092c1
Ali-Reza-001 <alirezaahmadi008hero@gmail.com>
2025-10-30 14:51:34 +0430

    nus

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,12 +3,13 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY); 
 
 const sendEmail = async ({sender = 'admin@ali-reza.dev', subject, email, content}) => {
+  const htmlContent = content.replace(/\n/g, '<br>');
   try {
     await resend.emails.send({ 
       from: sender, 
       to: email, 
       subject, 
-      html: `<pre>${content}</pre>` 
+      html: `<div style="font-family: Arial, sans-serif; font-size: 14px;">${htmlContent}</div>`
     });
   } catch (error) {
     console.error('Email send failed:', error);
